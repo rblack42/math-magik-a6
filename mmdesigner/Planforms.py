5b52e7f2eb5d7f43b969da1b982027b5e6125969
Roie Black <roie.black@gmail.com>
2021-11-21 16:59:19 -0600

    Start on Planform code

---
+++ mmdesigner/Planforms.py
@@ -0,0 +1,28 @@
+class Planform(object):
+
+	def __init__(self):
+		self.dihedral_types = [
+			"flat",
+			"simple-dihedral",
+			"flat_polyhedral",
+			"general_polyhedral",
+		]
+		self.planforms = [
+			"rectangular",
+			"straight_taper_le",
+			"straight_taper_te",
+			"double_straight_taper",
+			"rectangular_taper_le"
+			"rectangular_taper_te",
+			"rectangular_double_taper",
+			"rectangular_round_le",
+			"rectangular_round_te",
+			"rectangular_round_tip",
+		]
+
+	def leading_edge(self, y):
+		"""return x coordinate got given y"""
+
+	def trailing_edge(self, y):
+		"""return x coordinate for given y"""
+			
